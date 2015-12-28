2138a1db97d09e2e8728c844e6989302a4f32b2c
Benjamin Mach <pincy55@gmail.com>
2015-12-28 20:28:47 +0100

    init

---
+++ EvE/Eveapi.pm
@@ -0,0 +1,252 @@
+package EvE::Eveapi 0.02;
+
+use strict;
+use warnings;
+use LWP::Simple;
+use JSON;
+use XML::Simple;
+use File::Copy;
+
+
+#** @file Eveapi.pm
+# @brief a simple lib for the EvE Online XML API
+#**
+
+#** @function new (args)
+# @brief constructor, may be called with with args hash
+#
+# Valid Fields in the optional args are:
+# 	- debug {0 == off, 1 == on}
+# 	- agent {string for the LWP::UserAgent}
+# 	- keyID
+# 	- vCode
+# 	- characterID
+# 	- file {filename to load API Keys from, uses loadFile}
+#**
+
+sub new {
+	my $proto = shift;
+	my $class = ref($proto) || $proto;
+	my $args = shift || undef;
+	my $self = {};
+	$self->{'debug'} = (defined($args->{debug}) ? $args->{debug} : 0);
+	$self->{'api_root'} = "https://api.eveonline.com";
+	$self->{'ua'} = LWP::UserAgent->new();
+	my $ua_str = (defined($args->{agent}) ?$args->{agent} : "perl_simple_api");
+	$self->{'ua'}->agent($ua_str);
+	$self->{keyID} = (defined($args->{keyID}) ?$args->{keyID} : undef);
+	$self->{vCode} = (defined($args->{vCode}) ?$args->{vCode} : undef);
+	$self->{characterID} = (defined($args->{characterID}) ?$args->{characterID} : undef);
+	$self->{'url'} = undef;
+	bless ($self, $class);
+	if (defined($args->{file})) {
+		$self = $self->loadFile($args->{file});
+	}
+	return $self;
+}
+
+#** @function attr (attr,val)
+# @brief sets/gets attribute
+#
+# If called with val, it sets the value of val in attr.
+# In any way, it returns the value of attr
+#**
+
+sub attr {
+	my ($self, $attr, $val) = @_;
+	if (! defined($self) || ! defined($attr)) { return undef; }
+	if (defined($val)) {
+		$self->{$attr} = $val;
+	}
+	return $self->{$attr};
+}
+
+#** @function loadFile (filename)
+# @brief loads the API credentials from filename
+#
+# Loads a json file, containing one or more API credentials.
+# for a single API key, it expects a hash_ref with keyID and vCode,
+# for multiple the hash_refs may be listed in an array
+# filename may be 
+# 	- string with filename
+# 	- array_ref with [filename,index]
+#	- hash_ref with {name => filename, index => x}
+#**
+
+sub loadFile {
+	my ($self, $name) = @_;
+	if (! defined($self)) { return dieError("called without self"); }
+	my ($f,$str,$index);
+	if (ref($name) eq "ARRAY") { ($name,$index) = @{ $name };}
+	if (ref($name) eq "HASH") {
+		$index = $name->{index};
+		$name = $name->{name} || $name->{filename} || undef;
+	}
+	if (! defined($index)) { $index = 0; }
+	if (! defined($name)) { return dieError("called without filename"); }
+	open($f, "< $name") or return dieError("Error on opening $name\n");
+	while (my $l = <$f>) { $str = "$str$l"; }
+	close($f);
+	my $ret = from_json($str) || undef;
+	if (defined($ret) && ref($ret) eq "ARRAY") { $ret = $ret->[$index]; }
+	if ($self->{debug}) { print "Read from file $name: $ret\n"; }
+	if (defined($ret->{keyID})) { $self->{keyID} = $ret->{keyID}; }
+	if (defined($ret->{vCode})) { $self->{vCode} = $ret->{vCode}; }
+	if ($self->{debug}) { print "Loaded: keyID: $self->{keyID}, vCode: $self->{vCode}\n"; }
+	return $self;
+}
+
+#** @function saveFile (name, data)
+# @brief save data or the current API credentials to name
+#
+# If data is omitted, the current API credentials are written to name.
+# In case name already exists, it will try to make a backup with the "bak"
+# extension
+#**
+
+sub saveFile {
+	my ($self, $name, $data) = @_;
+	if (! defined($self) || ! defined($name)) { return dieError("called without self or filename?"); }
+	my $f;
+	if ($self->{debug}) { print "Writing to file $name: $data\n"; }
+	if (-f $name) {
+		if ($self->{debug}) { print "File $name exists, creating backup\n"; }
+		if (syscopy($name, $name."bak") == 0) {
+			return dieError("could not backup existing file. Error: '$!'");
+		}
+	}
+	open($f, "> $name") or return dieError("Error on opening $name\n");
+	if (! defined($data)) {
+		$data = { keyID => $self->{keyID}, vCode => $self->{vCode} }
+	}
+	print $f to_json($data);
+	close($f);
+	return $self;
+}
+
+sub dieError {
+	my ($self, $msg) = @_;
+	if (! defined($msg)) { $msg = $self; }
+	print STDERR $msg;
+	return undef;
+}
+
+sub getchild {
+	my ($self, $arg) = @_;
+	if (! defined($arg) ) { $arg = $self; }
+	if (ref($arg) eq "HASH") {
+		my @keys = keys %{ $arg };
+		return [$keys[0], @{ getchild($arg->{$keys[0]}) } ];
+	}
+	return [$arg];
+}
+
+#** @function load (url)
+# @brief loads url, or tries to load self->{url}
+#
+#**
+
+sub load {
+	my ($self, $url) = @_;
+	if (! defined($self) || (! defined($url) && ! defined($self->{url}))) { return dieError("called without self or url"); }
+	if (defined($url)) { $self->{url} = $url; }
+	my $resp = $self->{ua}->get($self->{url});
+	if ($resp->is_success) {
+		my $content = $resp->content;
+		my $parser = new XML::Simple();
+		my $tmp = $parser->XMLin($content);
+		$self->{last} = $tmp;
+		return $tmp;
+	}
+	else {
+		return dieError("response was ".$resp->code." - ".$resp->message."\n");
+	}
+	return undef;
+}
+
+#** @function loadPath (arg)
+# @brief builds the url Path for the load function and calls it. plzusethis!!!
+#
+# arg needs to contain the path to be called. It may be:
+# 	- array_ref {e.g. [ "account", "APIKeyInfo" ] }
+# 	- hash_ref {e.g. { "account" => "APIKeyInfo" } }
+# If arg contains a character path and characterID was not set, it will be set to
+# the first characterID returned by the API
+#**
+
+sub loadPath {
+	my ($self, $arg) = @_;
+	if (! defined($self) || ! defined($arg)) { return dieError("called without self or path to call"); }
+	my $str = "/";
+	if (ref($arg) eq "HASH") {
+		$arg = getchild($arg);
+	} elsif (ref($arg) eq "ARRAY") {
+		my @arr = @{ $arg };
+		$str .= join("/", @arr);
+	}
+	else {
+		$str = $arg;
+	}
+	if ($str =~ /^char/i && ! defined($self->{characterID})) {
+		my $tmp = $self->loadPath(["Acccount","Characters"]);
+		if (ref($tmp->{result}{rowset}{row}) eq "ARRAY") {
+			$self->{characterID} = $tmp->{result}{rowset}{row}[0]{characterID};
+		}
+		else {
+			$self->{characterID} = $tmp->{result}{rowset}{row}{characterID};
+		}
+	}
+	$str .= ".xml.aspx?";
+	if ($str =~ /^\/(corp|char|account)/) {
+		if (! defined($self->{keyID}) || ! defined($self->{vCode})) {
+			dieError("keyID or vCode not set/loaded");
+		}
+		$str .= "keyID=".$self->{keyID}."&";
+		$str .= "vCode=".$self->{vCode};
+		if ($str =~ /^\/char/) {
+			if (! defined($self->{characterID})) {
+				dieError("characterID not set/loaded");
+			}
+			$str .= "&characterID=".$self->{characterID};
+		}
+	}
+	if ($self->{debug}) { print "dbg: query string: $str\n"; }
+	$self->{url} = $self->{api_root}.$str;
+	return $self->load();
+}
+
+###
+# static possible data export
+# - didn't find a dynamic way to get all api endpoints yet -
+# status 22.09.2015
+###
+sub getpossibles {
+	return {
+		account => [ "AccountStatus", "APIKeyInfo", "Characters" ],
+		api => [ "CallList" ],
+		char => [ "AccountBalance", "AssetList", "CalendarEventAttendees",
+			"CharacterSheet", "ContactList", "ContactNotifications", "Contracts",
+			"ContractItems", "ContractBids", 
+			"FacWarStats", "IndustryJobs", "Killlog", "Locations", "MailBodies",
+			"MailingLists", "MailMessages", "MarketOrders", "Medals",
+			"Notifications", "NotificationTexts", "Research", "SkillInTraining",
+			"SkillQueue", "Standings", "UpcomingCalendarEvents", "WalletJournal",
+			"WalletTransactions" ],
+		corp => [ "AccountBalance", "AssetList", "ContactList", "ContainerLog",
+			"Contracts", "ContractItems", "ContractBids", "CorporationSheet",
+			"FacWarStats", "IndustryJobs", "Killlog", "Locations", "MarketOrders",
+			"Medals", "MemberMedals", "MemberSecurity", "MemberSecurityLog",
+			"MemberTracking", "OutpostList", "OutpostServiceDetail",
+			"Shareholders", "Standings", "StarbaseDetail", "StarbaseList",
+			"Titles", "WalletJournal", "WalletTransactions" ],
+		eve => [ "AllianceList", "CertificateTree", "CharacterAffiliation",
+			"CharacterID", "CharacterInfo", "CharacterName",
+			"ConquerableStationList", "ErrorList", "FacWarStats",
+			"FacWarTopStats", "RefTypes", "SkillTree", "TypeName" ],
+		map => [ "FacWarSystems", "Jumps", "Kills", "Sovereignty",
+			"SovereigntyStatus" ],
+		server => [ "ServerStatus" ]
+	};
+}
+
+1;
---
+++ EvE/Evecrest.pm
@@ -0,0 +1,194 @@
+package EvE::Evecrest 0.03;
+
+use strict;
+use warnings;
+use LWP::UserAgent;
+use JSON;
+
+###
+# TODO: load partner href and different things than child href, SSO
+###
+#
+#** @file Evecrest.pm
+# @brief a simple lib for the EvE Online CREST API
+#
+# Missing Features:
+# 	- partner href and other things, than child href
+# 	- SSO
+#**
+
+#** @function new (args)
+# @brief constructor
+#
+# Valid Fields for the optional args are:
+# 	- debug {0 == off, 1 == on}
+# 	- agent {string for the LWP::UserAgent}
+# On creation, the root will be called and available for traversing on return.
+# The Evecrest object will allways contain the tree from the last call under the
+# {last} attribute.
+#**
+
+sub new {
+	my $proto = shift;
+	my $class = ref($proto) || $proto;
+	my $self = {};
+	my $arg = shift || undef;
+	$self->{'debug'} = (defined($arg->{debug}) ? $arg->{debug} : 0);
+	$self->{'crest_root'} = "https://public-crest.eveonline.com/";
+	$self->{'ua'} = LWP::UserAgent->new();
+	my $ua_str = (defined($args->{agent}) ?$args->{agent} : "perl_simple_crest");
+	$self->{'ua'}->agent($us_str);
+	$self->{'last'} = undef;
+	bless ($self, $class);
+	load_url($self, $self->{'crest_root'});
+	return $self;
+}
+
+#** @function load_url (url)
+# @brief justdontusethis!
+#
+#**
+
+sub load_url {
+	my ($self, $url) = @_;
+	if (!defined($self) || !defined($url)) {
+		return undef;
+	}
+	print "requesting $url\n" if $self->{'debug'} >= 2;
+	my $resp = $self->{'ua'}->get($url);
+	if ($resp->is_success) {
+		$self->{'last'} = from_json($resp->content);
+		return $self->{'last'};
+	}
+	else {
+		print "$url returned $resp->status_line \n No JSON:\n $resp->content \n" if $self->{'debug'} >= 1;
+		return undef;
+	}
+}
+
+#** @function load_child_href (arg)
+# @brief load the href of the child with name
+#
+# arg is expected to be a hash_ref with the field
+# name. The function will look on the current top of the tree
+# for this name and load its child href.
+# Optional field is tree, where another current top can be set
+#**
+
+sub load_child_href {
+	my ($self, $arg) = @_;
+	if (!defined($self) || ! defined($arg)) {
+		return undef;
+	}
+	my $tree = $arg->{'tree'} if defined($arg->{'tree'});
+	my $name = $arg->{'name'} if defined($arg->{'name'});
+	if (!defined($self) || !defined($name)) {
+		return undef;
+	}
+	# using the tree from the last call
+	if (!defined($tree)) {
+		$tree = $self->{'last'} ;
+	}
+	# we're doing an initial call for the root
+	if (!defined($self->{'last'})) {
+		$self->{'last'} = $self->load_url($self->{'crest_root'});
+	}
+	else {
+		if (ref($name) eq "ARRAY") {
+			if (!defined($tree->{$name->[0]}{$name->[1]}{'href'})) {
+				print "$name->[0] - $name->[1] not found in current tree\n" if $self->{'debug'} >= 1;
+				return undef;
+			}
+			else {
+				$self->{'last'} = $self->load_url($tree->{$name->[0]}{$name->[1]}{'href'});
+			}
+		}
+		else {
+			if (!defined($tree->{$name}{'href'})) {
+				my $index = $self->search_in_array({ search => $name, arr => $tree->{'items'} });
+				if (ref($index) eq "ARRAY") {
+					if (defined($tree->{'items'}[$index->[0]]{$index->[1]}) &&
+						defined($tree->{'items'}[$index->[0]]{'href'})) {
+						$self->{'last'} = $self->load_url($tree->{'items'}[$index->[0]]{'href'});
+					}
+				} else {
+					print "$name not found in current tree\n" if $self->{'debug'} >= 1;
+					return undef;
+				}
+			}
+			else {
+				$self->{'last'} = $self->load_url($tree->{$name}{'href'});
+			}
+		}
+	}
+	return $self->{'last'};
+}
+
+#** @function load_child_multipage (arg)
+# @brief same as load_child_href, but for multipage targets
+#
+#**
+
+sub load_child_multipage {
+	my ($self, $arg) = @_;
+	if (!defined($self) || ! defined($arg)) {
+		return undef;
+	}
+	my $tree = $arg->{'tree'} if defined($arg->{'tree'});
+	my $name = $arg->{'name'} if defined($arg->{'name'});
+	if (!defined($self) || !defined($name)) {
+		return undef;
+	}
+	# using the tree from the last call
+	if (!defined($tree)) {
+		$tree = $self->{'last'} ;
+	}
+	my @items = ();
+	do {
+		$tree = $self->load_child_href({
+				tree => $tree,
+				name => $name,
+			});
+		@items = (@items, @{$self->{'last'}{'items'}});
+		$name = "next";
+	} while (defined($self->{'last'}{'next'}));
+	$self->{'last'}{'items'} = \@items;
+	return $self->{'last'};
+}
+
+sub search_in_array {
+	my ($self, $arg) = @_;
+	if (!defined($self) && ! defined($arg)) {
+		return undef;
+	}
+	my $search = $arg->{'search'} if defined($arg->{'search'});
+	my $arr = $arg->{'arr'} if defined($arg->{'arr'});
+	if (ref($arg) ne "HASH") {
+		$search = $self;
+		$arr = $arg;
+	}
+	if (!defined($arr)) {
+		foreach my $k (keys %{$self->{'last'}}) {
+			if (ref($self->{'last'}{$k}) eq "ARRAY") {
+				$arr = $self->{'last'}{$k};
+				last;
+			}
+		}
+	}
+	for (my $i=0; $i <= $#{$arr}; $i++) {
+		if (ref($arr->[$i]) eq "HASH") {
+			foreach my $k (keys %{$arr->[$i]}) {
+				if ($k =~ /$search/) {
+					return $i;
+				}
+				elsif ($arr->[$i]{$k} =~ /$search/) {
+					return [$i, $k];
+				}
+			}
+		}
+	}
+	return undef;
+}
+
+
+1;
